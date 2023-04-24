eed32438ef0e0f5999819484f83b4de436779167
MAGNINAntoine <amagnin@gaming.tech>
2023-04-24 16:04:13 +0200

    oui

--- navalBattleMulti/Scene.cpp
+++ navalBattleMulti/Scene.cpp
@@ -1,4 +1,32 @@
 #include "Framework.h"
+enum class GridType
+{
+	Empty,
+	Ship,
+	Hit,
+	Miss
+};
+
+enum class ShipType
+{
+	PorteAvion,
+	Croiseur,
+	ContreTorpileur,
+	SousMarin,
+	Torpilleur
+};
+
+struct Grid
+{
+	GridType type = GridType::Empty;
+	sf::RectangleShape rect;
+};
+
+struct Ship
+{
+	int size;
+	std::vector<Grid*> cells;
+};
 
 Scene::Scene()
 {
@@ -7,6 +35,8 @@ Scene::Scene()
 Scene::Scene(sf::RenderWindow* window)
 {
 	m_window = window;
+	Ship ship;
+	ship.size = 5; 
 }
 
 Scene::~Scene()
