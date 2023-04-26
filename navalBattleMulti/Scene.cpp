e4dcb48bda8a944eab036c526e61c52585f22f67
MAGNINAntoine <amagnin@gaming.tech>
2023-04-26 14:49:03 +0200

    Fix scene

--- navalBattleMulti/Scene.cpp
+++ navalBattleMulti/Scene.cpp
@@ -1,32 +1,4 @@
 #include "Framework.h"
-enum class GridType
-{
-	Empty,
-	Ship,
-	Hit,
-	Miss
-};
-
-enum class ShipType
-{
-	PorteAvion,
-	Croiseur,
-	ContreTorpileur,
-	SousMarin,
-	Torpilleur
-};
-
-struct Grid
-{
-	GridType type = GridType::Empty;
-	sf::RectangleShape rect;
-};
-
-struct Ship
-{
-	int size;
-	std::vector<Grid*> cells;
-};
 
 Scene::Scene()
 {
@@ -35,8 +7,6 @@ Scene::Scene()
 Scene::Scene(sf::RenderWindow* window)
 {
 	m_window = window;
-	Ship ship;
-	ship.size = 5;
 }
 
 Scene::~Scene()
